f8adc83dcd321036d7f9ff99b302163ec673aeae
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-10-13 00:10:40 +0530

    miss env declared

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,10 +1,11 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000,
-    ATLAS_DB_URL: process.env.ATLAS_DB_URL ,
-    LOG_DB_URL: process.env.LOG_DB_URL,
-    NODE_ENV: process.env.NODE_ENV || "development"
-}
+  PORT: process.env.PORT || 3000,
+  ATLAS_DB_URL: process.env.ATLAS_DB_URL,
+  LOG_DB_URL: process.env.LOG_DB_URL,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  FE_URL: process.env.FE_URL,
+};
